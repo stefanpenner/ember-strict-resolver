62d4d1d23af7f75d93078b28a5984033d118216e
Quinn C. Hoyer <quinn@themightyquinn.com>
2017-06-03 16:08:39 -0700

    Upgrade ember-cli

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,10 +1,9 @@
 /* eslint-env node */
-'use strict';
 
 module.exports = function(environment) {
-  let ENV = {
+  var ENV = {
     modulePrefix: 'dummy',
-    environment,
+    environment: environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
