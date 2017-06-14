7a36aa61260dee6dd345e9f4ac3e7a98ba60168a
Quinn C. Hoyer <quinn@themightyquinn.com>
2017-06-14 13:29:06 -0700

    Update config/ember-try.js

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,4 +1,67 @@
 /* eslint-env node */
 module.exports = {
-  useVersionCompatibility: true
+  "scenarios": [
+    {
+      "name": "default",
+      "bower": {
+        "dependencies": {}
+      }
+    },
+    {
+      "name": "ember-beta",
+      "allowedToFail": true,
+      "bower": {
+        "dependencies": {
+          "ember": "components/ember#beta"
+        },
+        "resolutions": {
+          "ember": "beta"
+        }
+      }
+    },
+    {
+      "name": "ember-canary",
+      "allowedToFail": true,
+      "bower": {
+        "dependencies": {
+          "ember": "components/ember#canary"
+        },
+        "resolutions": {
+          "ember": "canary"
+        }
+      }
+    },
+    {
+      "name": "ember-2.13.3",
+      "bower": {
+        "dependencies": {
+          "ember": "2.13.3"
+        }
+      }
+    },
+    {
+      "name": "ember-2.12.2",
+      "bower": {
+        "dependencies": {
+          "ember": "2.12.2"
+        }
+      }
+    },
+    {
+      "name": "ember-2.11.3",
+      "bower": {
+        "dependencies": {
+          "ember": "2.11.3"
+        }
+      }
+    },
+    {
+      "name": "ember-2.10.2",
+      "bower": {
+        "dependencies": {
+          "ember": "2.10.2"
+        }
+      }
+    }
+  ]
 };
